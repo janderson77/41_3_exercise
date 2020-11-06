fd150f09c697a75f16174f2db5c35fee55604e68
James <jamesseventyseven@gmail.com>
2020-11-06 22:55:26 +0000

    finished

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import JokeListClass from "./JokeList";
+
+function App() {
+  return (
+    <div className="App">
+      <JokeListClass />
+    </div>
+  );
+}
+
+export default App;
